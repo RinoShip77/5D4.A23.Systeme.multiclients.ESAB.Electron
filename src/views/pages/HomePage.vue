eabe69257509c71a996b6641b65265e796d195a6
SMoussette <89607408+Sebastien-Moussette@users.noreply.github.com>
2023-10-24 17:03:49 -0400

    Update HomePage.vue
    Added TODOs.

--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -3,6 +3,7 @@
         <div>
             <h2>Bienvenue sur </h2>
             <h1>Andromia Technologies</h1>
+            //TODO: Short description?
         </div>
         <div class="col-12">
             <button>Se connecter</button>
@@ -11,11 +12,12 @@
         </div>
     </DefaultLayout>
 </template>
-
+//TODO: Use different layout from the default in order to remove the NavBar.
 <script setup lang="ts">
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 </script>
 
+//TODO: Make the style for the page.
 <style scoped>
 button {
     width: 200px;
