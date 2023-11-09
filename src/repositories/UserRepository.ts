eef0efa42fb3f5fafc2c080f318ec1613e8819c4
SMoussette <89607408+Sebastien-Moussette@users.noreply.github.com>
2023-11-09 15:38:58 -0500

    Update UserRepository.ts

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -4,18 +4,26 @@ import { AxiosStatic } from 'axios';
 export class UserRepository {
     axios = inject('axios') as AxiosStatic;
 
-    public async login(username: string, password: string) {
+    public async login(user: string, password: string) {
 
         const data = {
-            username,
-            password
-        }
+            username: user,
+            password: password,
+        };
 
         return this.axios.post('http://localhost:3001/connexion', data);
     }
     /*public async CreateAccount() {
+
+        const data = {
+            username: user,
+            password: password,
+            email: email,
+
+        };
+
         try {
-            const res = await this.axios.post('http://localhost:3001/explorers');
+            const res = await this.axios.post('http://localhost:3001/explorers', data);
 
             if (res.status === 201) {
                 return res;
