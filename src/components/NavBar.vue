3d712330c5f9ba00bafe62a0a946f4ed0744d634
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-11-10 15:36:32 -0500

    final update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
         <router-link class="navbar-brand d-flex align-items-center" :to="{ name: 'allies' }">
           <img src="@/assets/logo.png" alt="Andromia Technologies" title="Andromia Technologies" class="img-fluid"
             width="100">
-          <h1 class="ms-2">Andromia Technologies</h1>
+          <h1 class="ms-2 title">Andromia Technologies</h1>
         </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
           aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,10 +17,9 @@
               <!-- <div class="display-3">
                 <i class="fa-solid fa-caret-left text-danger" style="margin-right: -17px"></i>
               </div> -->
-              <button class="btn btn-danger rounded-3 mt-1 px-5 fs-5 text-uppercase fw-bold">
-                <router-link class="navbar-brand d-flex align-items-center" :to="{ name: 'login' }">Se
-                  déconnecter</router-link>
-              </button>
+              <router-link class="btn btn-danger rounded-3 mt-1 px-5 fs-5 text-uppercase fw-bold" :to="{ name: 'login' }">
+                 Se déconnecter
+                </router-link>
               <div class="nav-link mx-2">
                 <img src="https://ui-avatars.com/api/?name=Firstname+Lastname&rounded=true&size=80">
                 <div style="margin-top: -1em; margin-left: 3.5em">
@@ -30,17 +29,17 @@
                 </div>
               </div>
             </div>
-            <div class="d-flex justify-content-between align-items-center bg-light rounded-4 px-5 py-2">
+            <div class="d-flex justify-content-between align-items-center bg-light rounded-4 px-5 py-2 stats">
               <div class="d-flex flex-column text-center mt-2 me-2">
                 <i class="fas fa-sack-dollar fs-1"></i>
                 <div title="Inox total">
-                  <span class="stats">99,99</span>
+                  <span>99,99</span>
                   <img src="@/assets/ui/inox.png" alt="Inox icon" class="img-fluid" width="50">
                 </div>
               </div>
               <div class="d-flex flex-column text-center ms-2">
                 <i class="fas fa-map-location-dot fs-1"></i>
-                <span class="stats mt-2">50</span>
+                <span class="mt-2">50</span>
               </div>
             </div>
           </div>
